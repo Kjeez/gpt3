8d1cbbe67c75891a4ec90a44839f028e3e6eb136
Kjeez <86542567+Kjeez@users.noreply.github.com>
2023-09-05 12:14:16 +0530

    new commit
    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.js',       // Include JavaScript files
+    './public/**/*.html',  // Include HTML files in the public directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
